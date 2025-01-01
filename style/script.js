5dd23a0fb85dd09a56f16eb83427e9b6cfd0ea8d
irfanmaiyola30 <100965341+irfanmaiyola30@users.noreply.github.com>
2025-01-02 01:19:09 +0700

    Update script.js

--- style/script.js
+++ style/script.js
@@ -83,3 +83,8 @@ document.addEventListener("DOMContentLoaded", function () {
             console.error("Error fetching IP address: ", error);
         });
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+    const audio = document.getElementById("background-music");
+    audio.volume = 0.5; 
+});
